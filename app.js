0b406861a9a93c58a75d6e5b2cb1b43200202f11
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-12-24 23:15:44 +0200

    Installed nodemon, morgan.

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
+const logger = require('morgan');
+
 const contactsRouter = require('./routers/contactsRouter/contactsRouter');
 
 const phoneBookBackend = express();
 
 phoneBookBackend.use(express.json());
+phoneBookBackend.use(logger('combined'));
 
 phoneBookBackend.use((req, res, next) => {
   console.log('This is my own middleware');
