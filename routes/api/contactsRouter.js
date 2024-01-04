05de707ad2d3c8637beff1bd8d16da688f09d914
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2024-01-04 11:42:19 +0200

    Smth wrong with commit

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -9,11 +9,6 @@ contactsRouter.get('/:contactId', getContactById);
 
 contactsRouter.post('/:contactId', validateBody(contactsSchemaRouter), addContact);
 
-      message: `Here will be all contacts by user id = ${contactId}`,
-      contactById: contactById,
-    });
-  } catch (error) {
-    RequestError(404);
 contactsRouter.delete('/:contactId', deleteContactById);
 
 module.exports = contactsRouter;
